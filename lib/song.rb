57b2263e27b88baea1c0ba9bb5376f3df70c7f59
bitterfly <dageneva@gmail.com>
2015-01-09 23:30:57 +0200

    Add initialize to Song class. Require new gem - taglib for song properties

--- lib/song.rb
+++ lib/song.rb
@@ -1,2 +1,20 @@
+require "taglib"
+require "rubygame"
+
 module MehPlayer
+  class Song
+    attr_reader :title, :artist, :album, :length, :filename
+    def initialize(filename)
+      @filename = filename
+      TagLib::FileRef.open(filename) do |fileref|
+        unless fileref.null?
+          tag = fileref.tag
+          @title = tag.title
+          @artist = tag.artist
+          @album = tag.album
+          @length = fileref.audio_properties.length
+        end
+      end
+    end
+  end
 end
