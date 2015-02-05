8e789d6c2d2727ab0f79afccab89b535f2fe56f6
bitterfly <dageneva@gmail.com>
2015-02-05 23:22:53 +0200

    Rubocopped almost everything - Size of classes is still a problem

--- lib/MehPlayer/gui/listwindow.rb
+++ lib/MehPlayer/gui/listwindow.rb
@@ -5,10 +5,11 @@ require 'MehPlayer/player'
 require 'Qt'
 
 module MehPlayer
-  module Gui  
+  module Gui
     class ListWindow < Qt::MainWindow
-      slots 'choose_song()', 'search()', 'enqueue_file()', 'enqueue_folder()', 'remove()', 'delete_all()',
-            'sort_by_author()', 'sort_by_title()', 'sort_by_rating()'
+      slots 'choose_song()', 'search()', 'enqueue_file()', 'enqueue_folder()',
+            'remove()', 'delete_all()', 'sort_by_author()', 'sort_by_title()',
+            'sort_by_rating()'
 
       def initialize(player, parent = nil)
         super(parent)
@@ -16,43 +17,106 @@ module MehPlayer
         @ui = Ui_ListWindow.new
         @ui.setupUi(self)
         @song_list_stylesheet = @ui.song_list.styleSheet
-        connect(@ui.song_list, SIGNAL('itemDoubleClicked(QListWidgetItem* )'), self, SLOT('choose_song()'))
-        connect(@ui.search, SIGNAL('textChanged(const QString &)'), self, SLOT('search()'))
-        connect(@ui.enqueue_file, SIGNAL('clicked()'), self, SLOT('enqueue_file()'))
-        connect(@ui.enqueue_folder, SIGNAL('clicked()'), self, SLOT('enqueue_folder()'))
-        connect(@ui.remove, SIGNAL('clicked()'), self, SLOT('remove()'))
-        connect(@ui.delete_all, SIGNAL('clicked()'), self, SLOT('delete_all()'))
-        connect(@ui.sort_by_author, SIGNAL('clicked()'), self, SLOT('sort_by_author()'))
-        connect(@ui.sort_by_title, SIGNAL('clicked()'), self, SLOT('sort_by_title()'))
-        connect(@ui.sort_by_rating, SIGNAL('clicked()'), self, SLOT('sort_by_rating()'))
+        connect_general_buttons
+        connect_open_buttons
+        connect_delete_buttons
+        connect_sort_buttons
+      end
+
+      def connect_general_buttons
+        connect(
+          @ui.song_list,
+          SIGNAL('itemDoubleClicked(QListWidgetItem* )'),
+          self, SLOT('choose_song()')
+        )
+        connect(
+          @ui.search,
+          SIGNAL('textChanged(const QString &)'),
+          self, SLOT('search()')
+        )
+      end
+
+      def connect_open_buttons
+        connect(
+          @ui.enqueue_file,
+          SIGNAL('clicked()'),
+          self, SLOT('enqueue_file()')
+        )
+        connect(
+          @ui.enqueue_folder,
+          SIGNAL('clicked()'),
+          self, SLOT('enqueue_folder()')
+        )
+      end
+
+      def connect_delete_buttons
+        connect(
+          @ui.remove,
+          SIGNAL('clicked()'),
+          self, SLOT('remove()')
+        )
+        connect(
+          @ui.delete_all,
+          SIGNAL('clicked()'),
+          self, SLOT('delete_all()')
+        )
+      end
+
+      def connect_sort_buttons
+        connect(
+          @ui.sort_by_author,
+          SIGNAL('clicked()'), self,
+          SLOT('sort_by_author()')
+        )
+        connect(
+          @ui.sort_by_title,
+          SIGNAL('clicked()'), self,
+          SLOT('sort_by_title()')
+        )
+        connect(
+          @ui.sort_by_rating,
+          SIGNAL('clicked()'), self,
+          SLOT('sort_by_rating()')
+        )
       end
 
       def color(index)
-        colors = ["157, 167, 181",
-                  "163, 190, 135",
-                  "170, 209, 102"] 
-        @ui.centralwidget.styleSheet = "background:rgb(%s)" % colors[index]
+        colors = ['157, 167, 181',
+                  '163, 190, 135',
+                  '170, 209, 102']
+        @ui.centralwidget.styleSheet = format(
+          'background:rgb(%s)', colors[index]
+        )
       end
 
       def load_icons(folder)
-        @ui.song_list.styleSheet = @song_list_stylesheet % {folder: File.dirname(__FILE__), subfolder: folder}
+        @ui.song_list.styleSheet = format(
+          @song_list_stylesheet,
+          folder: File.dirname(__FILE__), subfolder: folder
+        )
       end
 
       def songs=(playlist)
         @ui.song_list.clear
-        playlist.each_with_index do |song, index|
-          Qt::ListWidgetItem.new(song.to_s, @ui.song_list).setData(Qt::UserRole, Qt::Variant.new(@player.playlist.index(song)))
+        playlist.each_with_index do |song, _index|
+          Qt::ListWidgetItem.new(song.to_s, @ui.song_list)
+            .setData(Qt::UserRole, Qt::Variant
+            .new(@player.playlist.index(song)))
         end
       end
 
       def remove
-        selected_song = @ui.song_list.takeItem(@ui.song_list.currentRow()).data(Qt::UserRole).to_i
+        selected_song = @ui.song_list.takeItem(@ui.song_list.currentRow)
+                        .data(Qt::UserRole)
+                        .to_i
         @player.playlist.delete_at(selected_song)
         self.songs = @player.playlist
         if @player.playlist.empty?
           parent.stop
         else
-          @player.play(@player.current_song) if @player.current_song == selected_song
+          if @player.current_song == selected_song
+            @player.play(@player.current_song)
+          end
         end
       end
 
@@ -64,36 +128,38 @@ module MehPlayer
 
       def choose_song
         parent.bright_screen
-        @player.play(@ui.song_list.currentItem().data(Qt::UserRole).to_i)
+        @player.play(@ui.song_list.currentItem.data(Qt::UserRole).to_i)
       end
 
       def search
         keywords = @ui.search.text.split
-        self.songs = ((@player.find_by_description(keywords) | @player.find_by_info(keywords)) or @player.playlist)
+        self.songs = (
+          (@player.find_by_description(keywords) |
+          @player.find_by_info(keywords)) ||
+          @player.playlist
+        )
       end
 
       def enqueue_file
-        fileName = Qt::FileDialog.getOpenFileName(self)
-        if !fileName.nil? and Song.audio_file?(fileName)
-          playlist = Playlist.new([Song.new(fileName)])
-          @player.playlist += playlist.songs
-          self.songs = @player.playlist
-        end
+        file_name = Qt::FileDialog.getOpenFileName(self)
+        return unless !file_name.nil? && Song.audio_file?(file_name)
+        playlist = Playlist.new([Song.new(file_name)])
+        @player.playlist += playlist.songs
+        self.songs = @player.playlist
       end
 
       def enqueue_folder
-        folderName = Qt::FileDialog.getExistingDirectory(self)
-        if !folderName.nil?
-          playlist = Playlist.new
-          playlist.scan_folder(folderName)
-          @player.playlist += playlist.songs
-          self.songs = @player.playlist
-        end
+        folder_name = Qt::FileDialog.getExistingDirectory(self)
+        return if folder_name.nil?
+        playlist = Playlist.new
+        playlist.scan_folder(folder_name)
+        @player.playlist += playlist.songs
+        self.songs = @player.playlist
       end
 
       def sort_by_title
         self.songs = @player.playlist.sort! do |x, y|
-          if (x.title == y.title) 
+          if (x.title == y.title)
             (x.artist <=> y.artist)
           else
             (x.title <=> y.title)
@@ -103,7 +169,7 @@ module MehPlayer
 
       def sort_by_author
         self.songs = @player.playlist.sort! do |x, y|
-          if (x.artist == y.artist) 
+          if (x.artist == y.artist)
             (x.title <=> y.title)
           else
             (x.artist <=> y.artist)
@@ -116,7 +182,6 @@ module MehPlayer
           y.rate <=> x.rate
         end
       end
-
     end
   end
 end
--- lib/MehPlayer/gui/mainwindow.rb
+++ lib/MehPlayer/gui/mainwindow.rb
@@ -6,66 +6,91 @@ require 'MehPlayer/player'
 require 'yaml'
 
 module MehPlayer
-  module Gui  
+  module Gui
     class MainWindow < Qt::MainWindow
-
-      slots 'open_file()', 'open_folder()', 'play()', 'stop()', 'seek()', 'color()',
-            'volume()', 'mute()', 'next()', 'prev()', 'shuffle()', 'show_list()',
-            'save_playlist()', 'open_playlist()', 'set_description()', 'repeat()', 'rate()'
+      slots 'open_file()', 'open_folder()', 'play()', 'stop()', 'seek()',
+            'color()', 'volume()', 'mute()', 'next()', 'prev()', 'shuffle()',
+            'show_list()', 'save_playlist()', 'open_playlist()', 'rate()',
+            'repeat()', 'set_description()'
 
       def initialize(parent = nil)
         super(parent)
         @ui = Ui_MainWindow.new
         @ui.setupUi(self)
         @player = Player.new do
-          unless @ui.mute.checked?
-            @player.action.volume = @ui.volume.value.to_f/100
-          else
+          if @ui.mute.checked?
             mute
+            set_current_song_preferences
+          else
+            @ui.slider.maximum = @player.playlist[@player.current_song].length
+            @ui.slider.value = @player.seek
+            @ui.artist.text = @player.playlist[@player.current_song].artist
+            @ui.title.text = @player.playlist[@player.current_song].title
+            @ui.album.text = @player.playlist[@player.current_song].album
+            @ui.rating.text = '-' +
+                              @player.playlist[@player.current_song]
+                              .rate.to_s + '★-'
+            @ui.track.text = '(' +
+                             @player.playlist[@player.current_song]
+                             .track.to_s + ')'
+            @ui.rate.value = @player.playlist[@player.current_song].rate
           end
-          @ui.slider.maximum = @player.playlist[@player.current_song].length
-          @ui.slider.value = @player.seek
-          @ui.artist.text = @player.playlist[@player.current_song].artist
-          @ui.title.text = @player.playlist[@player.current_song].title
-          @ui.album.text = @player.playlist[@player.current_song].album
-          @ui.rating.text = '-' + @player.playlist[@player.current_song].rate.to_s + '★' +'-' 
-          @ui.track.text = '(' + @player.playlist[@player.current_song].track.to_s + ')'
-          @ui.rate.value = @player.playlist[@player.current_song].rate
         end
+        set_style_sheets
+        @list = ListWindow.new(@player, self)
+        set_skin_colors
+        color
+        set_icons
+        flatten_buttons
+        connect_play_open
+        connect_volume_progress
+        connect_specials
+      end
 
+      def set_style_sheets
         @mute_stylesheet = @ui.mute.styleSheet
         @shuffle_stylesheet = @ui.shuffle.styleSheet
         @repeat_stylesheet = @ui.repeat.styleSheet
         @screen_stylesheet = @ui.horizontalFrame_2.styleSheet
-        @list = ListWindow.new(@player, self)
-        @colors = [["sky", "139, 188, 175"],
-                  ["main", "88, 127, 122"], 
-                  ["mellon", "255, 146, 149"]]
-        @skin = 0
-        color
-        set_icons
-        flatten_buttons
-        connect_buttons
       end
 
-      def connect_buttons
-        connect(@ui.show_list, SIGNAL('clicked()'), self, SLOT('show_list()'))
-        connect(@ui.open_file, SIGNAL('clicked()'), self, SLOT('open_file()'))
-        connect(@ui.open_folder, SIGNAL('clicked()'), self, SLOT('open_folder()'))
-        connect(@ui.play_button, SIGNAL('clicked()'), self, SLOT('play()'))
-        connect(@ui.stop_button, SIGNAL('clicked()'), self, SLOT('stop()'))
-        connect(@ui.slider, SIGNAL('sliderReleased()'), self, SLOT('seek()'))
+      def connect_volume_progress
         connect(@ui.volume, SIGNAL('sliderReleased()'), self, SLOT('volume()'))
         connect(@ui.mute, SIGNAL('stateChanged(int)'), self, SLOT('mute()'))
+        connect(@ui.slider, SIGNAL('sliderReleased()'), self, SLOT('seek()'))
+      end
+
+      def connect_play_open
+        connect(@ui.play_button, SIGNAL('clicked()'), self, SLOT('play()'))
+        connect(@ui.stop_button, SIGNAL('clicked()'), self, SLOT('stop()'))
         connect(@ui.next, SIGNAL('clicked()'), self, SLOT('next()'))
-        connect(@ui.prev, SIGNAL('clicked()'), self, SLOT('prev()'))
-        connect(@ui.shuffle, SIGNAL('stateChanged(int)'), self, SLOT('shuffle()'))
-        connect(@ui.save_playlist, SIGNAL('clicked()'), self, SLOT('save_playlist()'))
-        connect(@ui.open_playlist, SIGNAL('clicked()'), self, SLOT('open_playlist()'))
-        connect(@ui.set_description, SIGNAL('clicked()'), self, SLOT('set_description()'))
-        connect(@ui.repeat, SIGNAL('clicked()'), self, SLOT('repeat()'))
-        connect(@ui.color, SIGNAL('clicked()'), self, SLOT('color()'))
+        connect(
+          @ui.prev, SIGNAL('clicked()'), self, SLOT('prev()')
+        )
+        connect(
+          @ui.open_folder, SIGNAL('clicked()'), self, SLOT('open_folder()')
+        )
+        connect(@ui.open_file, SIGNAL('clicked()'), self, SLOT('open_file()'))
+        connect(
+          @ui.open_playlist, SIGNAL('clicked()'), self, SLOT('open_playlist()')
+        )
+        connect(
+          @ui.save_playlist, SIGNAL('clicked()'), self, SLOT('save_playlist()')
+        )
+      end
+
+      def connect_specials
         connect(@ui.rate, SIGNAL('valueChanged(int)'), SLOT('rate()'))
+        connect(@ui.color, SIGNAL('clicked()'), self, SLOT('color()'))
+        connect(
+          @ui.shuffle, SIGNAL('stateChanged(int)'), self, SLOT('shuffle()')
+        )
+        connect(@ui.repeat, SIGNAL('clicked()'), self, SLOT('repeat()'))
+        connect(
+          @ui.set_description,
+          SIGNAL('clicked()'), self, SLOT('set_description()')
+        )
+        connect(@ui.show_list, SIGNAL('clicked()'), self, SLOT('show_list()'))
       end
 
       def flatten_buttons
@@ -80,11 +105,24 @@ module MehPlayer
         @ui.open_playlist.flat = true
         @ui.set_description.flat = true
       end
+
+      def set_skin_colors
+        @colors = [['sky', '139, 188, 175'],
+                   ['main', '88, 127, 122'],
+                   ['mellon', '255, 146, 149']]
+        @skin = 0
+      end
+
       def color
-        @skin = (@skin < @colors.size - 1) ? @skin + 1 : 0 
-        @ui.color.styleSheet = "background:rgb(%s)" % @colors[(@skin < @colors.size - 1) ? (@skin + 1) : 0][1]
-        @ui.widget.styleSheet = "background:rgb(%s)" % @colors[@skin][1]
-        @ui.horizontalFrame.styleSheet = "background:rgb(%s)" % @colors[@skin][1]
+        @skin = (@skin < @colors.size - 1) ? @skin + 1 : 0
+        @ui.color.styleSheet = format(
+          'background:rgb(%s)',
+          @colors[(@skin < @colors.size - 1) ? (@skin + 1) : 0][1]
+        )
+        @ui.widget.styleSheet = format('background:rgb(%s)', @colors[@skin][1])
+        @ui.horizontalFrame.styleSheet = format(
+          'background:rgb(%s)', @colors[@skin][1]
+        )
         load_icons(@colors[@skin][0])
         @list.color(@skin)
         bright_screen if @player.playing?
@@ -159,28 +197,26 @@ module MehPlayer
       end
 
       def open_file
-        fileName = Qt::FileDialog.getOpenFileName(self)
-          if !fileName.nil? and Song.audio_file?(fileName)
-            stop if @player.playing?
-            @playlist = Playlist.new([Song.new(fileName)])
-            @player.playlist = @playlist.songs
-            @list.songs = @player.playlist
-            @ui.info.hide
-            play_mode
-          end
+        file_name = Qt::FileDialog.getOpenFileName(self)
+        return if file_name.nil? || !Song.audio_file?(file_name)
+        stop if @player.playing?
+        @playlist = Playlist.new([Song.new(fileame)])
+        @player.playlist = @playlist.songs
+        @list.songs = @player.playlist
+        @ui.info.hide
+        play_mode
       end
 
       def open_folder
-        folderName = Qt::FileDialog.getExistingDirectory(self)
-          if !folderName.nil?
-            stop if @player.playing?
-            @playlist = Playlist.new
-            @playlist.scan_folder(folderName)
-            @player.playlist = @playlist.songs
-            @list.songs = @player.playlist
-            @ui.info.hide
-            play_mode
-          end
+        folder_name = Qt::FileDialog.getExistingDirectory(self)
+        return if folder_name.nil?
+        stop if @player.playing?
+        @playlist = Playlist.new
+        @playlist.scan_folder(folder_name)
+        @player.playlist = @playlist.songs
+        @list.songs = @player.playlist
+        @ui.info.hide
+        play_mode
       end
 
       def play_mode
@@ -192,17 +228,20 @@ module MehPlayer
       end
 
       def bright_screen
-        @ui.horizontalFrame_2.styleSheet = "background: rgb(255, 250, 255)"
+        @ui.horizontalFrame_2.styleSheet = 'background: rgb(255, 250, 255)'
         @ui.info.show
         pause_mode
       end
 
       def dead_screen
-        @ui.horizontalFrame_2.styleSheet = @screen_stylesheet % {folder: File.dirname(__FILE__), subfolder: @colors[@skin][0]}
+        @ui.horizontalFrame_2.styleSheet = format(
+          @screen_stylesheet,
+          folder: File.dirname(__FILE__), subfolder: @colors[@skin][0]
+        )
       end
 
       def play
-        if @player.playing? 
+        if @player.playing?
           if @player.paused?
             pause_mode
             @player.unpause
@@ -231,28 +270,29 @@ module MehPlayer
       end
 
       def volume
-        if @player.playing? 
+        if @player.playing?
           @ui.volume.enabled = true
-          @player.action.volume = @ui.volume.value.to_f/100
+          @player.action.volume = @ui.volume.value.to_f / 100
         else
           @ui.volume.enabled = false
         end
       end
 
       def mute
-        if @player.playing? 
-          if @ui.mute.checked?
-            @player.action.volume = 0
-            @ui.volume.enabled = false
-          else
-            @player.action.volume = @ui.volume.value.to_f/100
-            @ui.volume.enabled = true
-          end
+        return unless @player.playing?
+        if @ui.mute.checked?
+          @player.action.volume = 0
+          @ui.volume.enabled = false
+        else
+          @player.action.volume = @ui.volume.value.to_f / 100
+          @ui.volume.enabled = true
         end
       end
 
       def next
-        unless @player.playlist.empty?
+        if @player.playlist.empty?
+          stop
+        else
           @player.next_song
           if @player.current_song <= @player.playlist.size
             @player.play(@player.current_song)
@@ -260,63 +300,58 @@ module MehPlayer
           else
             if @player.shuffle
               @player.play(0)
-            else  
+            else
               @player.play(@player.playlist.size - 1)
             end
-          end
-        else
-          stop
-        end  
-      end
-      
-      def prev
-        unless @player.playlist.empty?
-          @player.prev_song
-          if @player.current_song >= 0 
-            @player.play(@player.current_song)
-            mute
-          else
-            @player.play(0)
           end
         end
       end
 
+      def prev
+        return if @player.playlist.empty?
+        @player.prev_song
+        if @player.current_song >= 0
+          @player.play(@player.current_song)
+          mute
+        else
+          @player.play(0)
+        end
+      end
+
       def shuffle
-        @player.shuffle = (not @player.shuffle)
+        @player.shuffle = (!@player.shuffle)
       end
-      
+
       def repeat
-        @player.repeat = (not @player.repeat)
+        @player.repeat = (!@player.repeat)
       end
+
       def show_list
         @list.show
       end
 
       def save_playlist
-        if @player.playlist
-          fileName = Qt::FileDialog.getSaveFileName(self)
-          File.write(fileName, @player.playlist.to_yaml)
-        end
+        return unless @player.playlist
+        file_name = Qt::FileDialog.getSaveFileName(self)
+        File.write(file_name, @player.playlist.to_yaml)
       end
 
       def open_playlist
-        fileName = Qt::FileDialog.getOpenFileName(self)
-          if !fileName.nil?
-            stop if @player.playing?
-            dead_screen
-            @player.playlist = YAML.load(File.read(fileName))
-            @list.songs = @player.playlist
-          end
+        file_name = Qt::FileDialog.getOpenFileName(self)
+        return if file_name.nil?
+        stop if @player.playing?
+        dead_screen
+        @player.playlist = YAML.load(File.read(file_name))
+        @list.songs = @player.playlist
       end
 
       def set_description
-        unless @player.playlist.empty?
-          description = Qt::InputDialog.getText(
-            self, @player.playlist[@player.current_song].title, 
-            "Enter description",Qt::LineEdit::Normal, 
-            @player.playlist[@player.current_song].description)
-            @player.playlist[@player.current_song].description = description
-        end
+        return if @player.playlist.empty?
+        description = Qt::InputDialog.getText(
+          self, @player.playlist[@player.current_song].title,
+          'Enter description', Qt::LineEdit::Normal,
+          @player.playlist[@player.current_song].description)
+        @player.playlist[@player.current_song].description = description
       end
 
       def rate
@@ -325,7 +360,7 @@ module MehPlayer
           @player.playlist[@player.current_song].rate = @ui.rate.value
         else
           @ui.rate.enabled = false
-        end  
+        end
       end
     end
   end
